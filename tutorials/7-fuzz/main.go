6988b9cc65e68ab981b431b64e9a891b436f0a83
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2024-01-04 11:25:55 +0800

    fix: `Reverse` use `rune`

--- tutorials/7-fuzz/main.go
+++ tutorials/7-fuzz/main.go
@@ -12,9 +12,9 @@ func main() {
 }
 
 func Reverse(s string) string {
-	b := []byte(s)
-	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
-		b[i], b[j] = b[j], b[i]
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
 	}
-	return string(b)
+	return string(r)
 }

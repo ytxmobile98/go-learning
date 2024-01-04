944c80350376c7e57bbf722fb443e99d5dc58433
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2024-01-04 11:27:38 +0800

    test: `Reverse` use `rune`, add debugging info

--- tutorials/7-fuzz/main.go
+++ tutorials/7-fuzz/main.go
@@ -12,7 +12,9 @@ func main() {
 }
 
 func Reverse(s string) string {
+	fmt.Printf("input: %q\n", s)
 	r := []rune(s)
+	fmt.Printf("runes: %q\n", r)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
 	}

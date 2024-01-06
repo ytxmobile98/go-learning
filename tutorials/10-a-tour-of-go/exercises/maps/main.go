08563dd0201b2ca7ea8c9829f5c3b320461820e5
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2024-01-06 12:25:03 +0800

    feat: maps

---
+++ tutorials/10-a-tour-of-go/exercises/maps/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	words := strings.Fields(s)
+	counts := make(map[string]int)
+
+	for _, word := range words {
+		counts[word]++
+	}
+
+	return counts
+}
+
+func main() {
+	wc.Test(WordCount)
+}

820317b1611fd1aeaae1a097caa5b638e64c3dd8
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2024-01-10 10:55:57 +0800

    feat: web crawler, added caching & parallel fetching

--- tutorials/10-a-tour-of-go/exercises/concurrency/2-web-crawler/main.go
+++ tutorials/10-a-tour-of-go/exercises/concurrency/2-web-crawler/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 type Fetcher interface {
@@ -10,25 +11,60 @@ type Fetcher interface {
 	Fetch(url string) (body string, urls []string, err error)
 }
 
+type SafeCache struct {
+	urls map[string]bool
+	mu   sync.RWMutex
+}
+
+func (c *SafeCache) Get(url string) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.urls[url]
+}
+
+func (c *SafeCache) Put(url string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.urls[url] = true
+}
+
 // Crawl uses fetcher to recursively crawl
 // pages starting with url, to a maximum of depth.
 func Crawl(url string, depth int, fetcher Fetcher) {
-	// TODO: Fetch URLs in parallel.
-	// TODO: Don't fetch the same URL twice.
-	// This implementation doesn't do either:
-	if depth <= 0 {
-		return
+	cache := &SafeCache{urls: make(map[string]bool)}
+	wg := &sync.WaitGroup{}
+
+	var crawl func(url string, depth int)
+
+	crawl = func(url string, depth int) {
+		defer wg.Done()
+
+		if depth <= 0 {
+			return
+		}
+		if cache.Get(url) {
+			return
+		}
+
+		cache.Put(url)
+		body, urls, err := fetcher.Fetch(url)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("found: %s %q\n", url, body)
+		for _, u := range urls {
+			wg.Add(1)
+			go crawl(u, depth-1)
+		}
 	}
-	body, urls, err := fetcher.Fetch(url)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("found: %s %q\n", url, body)
-	for _, u := range urls {
-		Crawl(u, depth-1, fetcher)
-	}
-	return
+
+	wg.Add(1)
+	go crawl(url, depth)
+
+	wg.Wait()
 }
 
 func main() {
